8c9a643aceb0381746ccfa60452c8494c582538f
Reinaldo <reinaldofavorettosantana@gmail.com>
2023-01-22 12:22:03 -0300

    Decimo segundo commit: validação em andamento

---
+++ 1st-challenge-scholarship-program-aws-react-main/1st-challenge-scholarship-program-aws-react-main/README.md
@@ -0,0 +1,2 @@
+# 1st-challenge-scholarship-program-aws-react
+This is my first internship challenge
---
+++ 1st-challenge-scholarship-program-aws-react-main/1st-challenge-scholarship-program-aws-react-main/script6.js
@@ -0,0 +1,12 @@
+function validar(){
+    const email = document.querySelector('.email').value;
+    const idade = document.querySelector('.nome').value;
+    const age = document.querySelector('.age').value;
+
+    if(nome === '')
+    console.log('Falta')
+    if(idade === '')
+    console.log('Falta')
+    if(age === '')
+    console.log('Falta')
+}
