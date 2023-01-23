53ca069baf7f5ff1964faba91f538282a718ed31
Reinaldo Favoretto Santana <reinaldofavorettosantana@gmail.com>
2023-01-23 12:46:32 -0300

    Decimo quinto commit: Validação 90% concluida

--- script3.js
+++ script3.js
@@ -1,18 +1,20 @@
 function validar(){
     var valid = false;
-    
-    var x = document.form.operate;
 
-    for(var i=0;i<x.lenght;i++){
-        if(x[i].checked){
-            valid = true;
-            break;
-        }
-    }
+    if(document.getElementById("operate-stock").checked){
+        valid = true}
+    else if(document.getElementById("operate-foreign").checked){
+        valid = true}
+    else if(document.getElementById("operate-commodity").checked){
+        valid = true}
+    else if(document.getElementById("operate-bond").checked){
+        valid = true}
+    else if(document.getElementById("operate-cryptocurrency").checked){
+        valid = true}
+
     if(valid){
         window.location.href = "index4.html";
     }
-    else{
-        alert('Select one')
-    }
+    else
+    alert('Select one')
 }
